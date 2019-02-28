27dc1fb9da1a2a6f07a812d9e9ab75a4cea34422
Fornito Marvin <marvin.fornito@gmail.com>
2019-02-28 15:44:08 +0100

    MCTS UCT simulation faite

--- src/modele/Noeud.java
+++ src/modele/Noeud.java
@@ -1,6 +1,7 @@
 package modele;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Noeud {
 
@@ -27,14 +28,49 @@ public class Noeud {
         enfant = new ArrayList<>();
     }
 
+    /*public Noeud selection() {
+        int filsPossible = 0;
+        if(this.state.getEnd()!= Puissance4.End.NO) {
+            return this;
+        }else{
+            for(int i = 0; i < 7 ; i++){
+                if(state.mouvementValide(i)){
+                    filsPossible++;
+                }
+            }
+            if(this.filsListe.isEmpty()||this.filsListe.size() < filsPossible){
+                return this;
+            }else{
+                double max =this.getFils(0).getBeta();
+                int selection = 0;
+                for(int i = 0; i < filsListe.size();i++){
+                    if(this.getFils(i).getBeta()>max){
+                        max = this.getFils(i).getBeta();
+                        selection = i;
+                    }
+                }
+                return getFils(selection).selection();
+            }
+        }
+    }*/
 
     // return un int correspondant
     // Etat.J1_GAGNE
     // Etat.J2_GAGNE
     // Etat.MATCH_NUL
-    /*public int simulation() {
-
-    }*/
+    public int simulation() {
+        Etat etatEncours=new Etat(this.etat);
+        int finPartie=etatEncours.testFin();
+        //Si la partie est finie
+        while (finPartie == Etat.PARTIE_EN_COURS) {
+            ArrayList<Coup> coupsPossibles=etatEncours.coupsPossibles();
+            Random random= new Random();
+            int indiceCoup=random.nextInt(coupsPossibles.size());
+            etatEncours.jouerCoup(coupsPossibles.get(indiceCoup));
+            finPartie=etatEncours.testFin();
+        }
+        return finPartie;
+    }
 
     //Est-ce un noeud joueur ou un noeud IA ?
     //true joueur
