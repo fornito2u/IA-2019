e4f20d359bd8b9d97c2e03f3887be35edc82186f
Fornito Marvin <marvin.fornito@gmail.com>
2019-02-28 11:16:23 +0100

    Création de la base des classes nécessaire au développement du jeu "Puissance 4".

--- src/modele/Noeud.java
+++ src/modele/Noeud.java
@@ -8,18 +8,35 @@ public class Noeud {
     private ArrayList<Noeud> enfant;
     private Etat etat;
     private int nbVictoires;
-    private int nbDefaites;
+    private int nbSimulation;
 
     public Noeud(Etat etat) {
-        this.etat = etat;
-        this.nbVictoires=0;
-
+        this.etat = new Etat(etat);
+        this.nbVictoires = 0;
+        this.nbSimulation = 0;
+        enfant = new ArrayList<>();
     }
+
+    public Noeud(Noeud noeudParent, Coup coup){
+        this.parent=noeudParent;
+        Etat etat=new Etat(this.parent.getEtat());
+        etat.jouerCoup(coup);
+        this.etat=etat;
+        this.nbVictoires = 0;
+        this.nbSimulation = 0;
+        enfant = new ArrayList<>();
+    }
+
+
 
     //Est-ce un noeud joueur ou un noeud IA ?
     //true joueur
     //false IA
     public boolean estNoeudHumain() {
         return (this.etat.getJoueur() == 1);
+    }
+
+    public Etat getEtat() {
+        return etat;
     }
 }
